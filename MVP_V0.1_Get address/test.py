00a937a4dd8a8dc52e9ccdd2cba01612eee6ba59
shawn0lee0 <robo_one@foxmail.com>
2015-12-08 16:13:21 +0800

    MVP V0.1 CLI get address

--- MVP_V0.1_Get address/test.py
+++ MVP_V0.1_Get address/test.py
@@ -4,7 +4,7 @@ bm=xBaiduMap()
 print bm.getLocation("天安门",'北京')
 print bm.getLocation("浙江大学玉泉校区")
 print bm.getLocation("浙江大学玉泉校区",'杭州')
-print bm.getAddress(39.915175, 116.403906)
+print bm.getAddress(39.912684,116.404049)
 
 
 
