9ba3df52837b6ac12d5f974ba2543c5aa917e35e
Robo_one <shawn0lee0@gmail.com>
2015-12-15 17:44:25 +0800

    MVPV0.3 pic Canny Edge to Contours

--- MVP_V0.2_contours/README.md
+++ MVP_V0.2_contours/README.md
@@ -33,3 +33,33 @@
 利用ndarray.tolist()可以将numpy的数组转为python的list
 效果如下：
 `[[[10, 18]], [[10, 249]], [[251, 249]], [[251, 18]]]`
+
+
+
+
+----------
+2015年12月15日
+
+因为直接使用contours方法提取的边框非常粗糙，而且会带来很多噪声。
+因此想到了下述方法：
+
+1. 载入图片
+2. 高斯降噪
+3. 二值化
+3. 灰度处理
+4. Canny边缘提取
+5. 讲提取的到边缘提取轮廊。
+
+注意几点，
+
+
+
+参考：
+
+[http://stackoverflow.com/questions/18074680/extract-single-line-contours-from-canny-edges](http://stackoverflow.com/questions/18074680/extract-single-line-contours-from-canny-edges)
+
+[OpenCV converting Canny edges to contours](http://www.helpsforcoder.com/code/15751940-opencv-converting-canny-edges-to-contours.html)
+
+效果图链接：[http://i4.tietuku.com/6c2f77ca748e17f1.jpg](http://i4.tietuku.com/6c2f77ca748e17f1.jpg)
+![](http://i4.tietuku.com/6c2f77ca748e17f1.jpg)
+
--- MVP_V0.2_contours/contours.py
+++ MVP_V0.2_contours/contours.py
@@ -9,32 +9,17 @@ import cv2.cv as cv
 import numpy
 position = 100
 
-img = cv2.imread('C:\\Users\\robo\\Desktop\\pyjpg\\kaizhi.jpg')
+img = cv2.imread('C:\\Users\\robo_one\\Desktop\\fx.jpg') #C:\Users\robo_one\Desktop\fx.jpg
 print type(img) #<type 'numpy.ndarray'>
 img = cv2.GaussianBlur(img,(3,3),0)
 print type(img)
-print (numpy.size(img)) #图像像素点-->685452
+print (numpy.size(img)) #高斯平滑后图像像素点-->685452
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 print (numpy.size(gray))#灰度图像像素点-->228484
-edge = cv.CreateImage((300, 300), 8, 1)
-print (numpy.size(edge))
-cv.Canny(gray, edge, position, position * 3, 3)
-ans = []
-for y in range(0, edge.shape[0]):
-    for x in range(0, edge.shape[1]):
-        if edge[y, x] != 0:
-            ans = ans + [[x, y]]
-ans = numpy.array(ans)
 
-print(ans.shape)
-print(ans[0:10, :])
-
-
-
-'''
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 cv2.imshow("gray", gray)
-cv.Canny(gray, edge, position, position * 3, 3) #边缘检测
+
 ret, binary = cv2.threshold(gray,127,255,cv2.THRESH_BINARY)
 contours, hierarchy = cv2.findContours(binary,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 cv2.drawContours(img,contours,-1,(0,0,255),3)
@@ -55,13 +40,12 @@ contours_list = contours[-1].tolist()
 print contours_list
 print contours_list[0][0]
 
-for i in contours_list:
- str_len_contours_list = str(len(contours_list[i]))
- print str_len_contours_list + "\n"
-
+img_pix = str(numpy.size(img))
+gray_pix = str(numpy.size(gray))
 #轮廊清单转文本输出
-s = open("WXJ_contours.txt",'a')
-s.write("contours num:" +"%s" %str_len_contours + "\n") 
+s = open("Contours.txt",'a')
+s.write("Img GaussianBlur pix nums:" +"%s" %img_pix + "\n") 
+s.write("Gausss to gray pix nums:" +"%s" %gray_pix + "\n") 
 s.write("contours num:" +"%s" %str_len_contours + "\n") 
 for ele in contours:
  s.write("%s\n" % ele)
@@ -76,5 +60,5 @@ l.write("**"*50  + "\n")
 l.close()
 
 cv2.imshow("img", img)
-'''
+
 cv2.waitKey(0)
