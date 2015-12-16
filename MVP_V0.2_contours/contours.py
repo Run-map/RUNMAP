841c84d0dc26350397818a6bd8f73dbd23040dcc
Robo_one <robo_one@foxmail.com>
2015-12-16 16:50:33 +0800

    MVP0.4_URL

--- MVP_V0.2_contours/contours.py
+++ MVP_V0.2_contours/contours.py
@@ -21,6 +21,7 @@ gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 cv2.imshow("gray", gray)
 
 ret, binary = cv2.threshold(gray,127,255,cv2.THRESH_BINARY)
+print type(binary)
 contours, hierarchy = cv2.findContours(binary,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 cv2.drawContours(img,contours,-1,(0,0,255),3)
 
